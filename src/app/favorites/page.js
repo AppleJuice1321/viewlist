407a1c5790784c36d6c0ebe68b381e1141c97d75
AppleJuice1321 <117173708+AppleJuice1321@users.noreply.github.com>
2024-08-07 10:30:28 +0200

    try again

--- src/app/favorites/page.js
+++ src/app/favorites/page.js
@@ -1,12 +1,36 @@
-"use client"
+"use client";
 
-import getIds from "@/actions/get-ids"
-import { useEffect } from "react"
+import getIds from "@/actions/get-ids";
+import PrimaryHeader from "@/components/primaryHeader";
+import SecondaryHeader from "@/components/secondaryHeader";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Favorites() {
-    useEffect(() => {
-        getIds().then(ids => console.log(ids))
-    },[])
+    const [data, setData] = useState({})
 
-    return null
+
+    useEffect(() => {
+        // getIds().then((ids) => console.log(ids));
+        const getFavoriteMovies = async () => {
+          const response = await axios.get(`https://api.themoviedb.org/3/account/21191127/favorite/movies`,       {
+            headers: {
+              Authorization: "Bearer " + process.env.NEXT_PUBLIC_READ_ACCESS_TOKEN,
+            },
+          })
+          setData(response.data)
+        }
+        getFavoriteMovies()
+      }, [])
+      console.log(data);
+
+  return (
+    <div>
+        <SecondaryHeader/>
+        <section>
+            
+        </section>
+        <PrimaryHeader/>
+    </div>
+  );
 }
